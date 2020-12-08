e45ccdd846e15a096cf9d5f3466fd02d03d2b538
Jessie Chen <yhcjessie1@gmail.com>
2020-12-07 22:28:34 -0500

    setup env file and install sementic UI REACT

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// semantic-ui-react
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
