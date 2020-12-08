71beffcd1ea63f9b48177a0b5c7fec7457cfb810
Jessie Chen <yhcjessie1@gmail.com>
2020-12-07 22:33:23 -0500

    install asios and setup SongContainer

---
+++ src/SongContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+class SongContainer extends Component {
+    constructor(props){
+        super(props);
+    
+        this.state = {
+          songs: []
+        }
+      }
+      componentDidMount(){
+        this.getSongs();
+      }
+      getSongs = async () => {
+        try {
+          const parsedSongs = await axios(
+            process.env.REACT_APP_FLASK_API_URL + '/api/v1/songs/'
+          );
+          console.log(parsedSongs.data.data);
+          await this.setState({
+            songs: parsedSongs.data.data,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      
+      render(){
+        return (
+          "I'm the SongContainer"
+          )
+      }
+}
+ 
+export default SongContainer;
